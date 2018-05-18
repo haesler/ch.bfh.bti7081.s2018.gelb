d769ce5cc66a940cde7c4ffa3d636445086aa137
haesler <haesler.marc@sunrise.ch>
2018-05-18 09:09:04 +0200

    commit

--- src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/components/login/LoginPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/components/login/LoginPresenter.java
@@ -1,26 +1,17 @@
 package ch.bfh.bti7081.s2018.yellow.health.ui.components.login;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.UI;
 import com.vaadin.ui.declarative.Design;
 
-import ch.bfh.bti7081.s2018.yellow.health.models.User;
-import ch.bfh.bti7081.s2018.yellow.health.repo.UserRepository;
-import ch.bfh.bti7081.s2018.yellow.health.ui.MyUI;
 import ch.bfh.bti7081.s2018.yellow.health.ui.layouts.LoginLayout;
+
 
 @SuppressWarnings("serial")
 public class LoginPresenter implements LoginView.LoginViewListener {
 	LoginLayout model;
     LoginView  view;
-    @Autowired
-    UserRepository repo;
 
 
 	public LoginPresenter(LoginLayout model, LoginView  view){
@@ -32,13 +23,9 @@ public class LoginPresenter implements LoginView.LoginViewListener {
 	}
 	
 	public void buttonClick(){
-		
-		String pw = repo.findPassword(view.getUsername());
-		
-		if( view.getPassword().equals(pw))
+		if( view.getUsername() == "demo" && view.getPassword() == "demo" )
 		{
 			view.setNotification("success");
-			((MyUI)UI.getCurrent()).isAuthenticated=true;
 		       //öffnen von Mainview...
 		} else {
 				view.setNotification("Login failed");
