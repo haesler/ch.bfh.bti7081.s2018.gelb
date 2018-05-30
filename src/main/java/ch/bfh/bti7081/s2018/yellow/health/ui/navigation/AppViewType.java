775dad823d50fcb2098c7faa27dd96f1878ca32a
kofep1 <37440690+kofep1@users.noreply.github.com>
2018-05-30 19:04:31 +0200

    Addcontact updated

--- src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/navigation/AppViewType.java
+++ src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/navigation/AppViewType.java
@@ -7,6 +7,7 @@ import com.vaadin.server.Resource;
 import ch.bfh.bti7081.s2018.yellow.health.ui.components.patient.AddPatientView;
 import ch.bfh.bti7081.s2018.yellow.health.ui.components.searchcontact.SearchContactView;
 import ch.bfh.bti7081.s2018.yellow.health.ui.components.searchpatient.SearchPatientView;
+import ch.bfh.bti7081.s2018.yellow.health.ui.components.contact.AddContactView;
 
 public enum AppViewType {
     CONTACT("Kontakte",SearchContactView.class, VaadinIcons.HOME),
