f416cb719f142842c07087bc67062dd844836464
ezurbf <fabian.zurbuchen@gmail.com>
2018-06-11 17:10:07 +0200

    -AddContactPresenter

--- src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/components/contact/AddContactPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2018/yellow/health/ui/components/contact/AddContactPresenter.java
@@ -1,33 +1,53 @@
 package ch.bfh.bti7081.s2018.yellow.health.ui.components.contact;
 
+
+import com.vaadin.ui.Notification;
 import ch.bfh.bti7081.s2018.yellow.health.models.Contact;
 import ch.bfh.bti7081.s2018.yellow.health.repo.ContactRepository;
+import ch.bfh.bti7081.s2018.yellow.health.ui.components.validate.ContactValidate;
 
 public class AddContactPresenter implements AddContactView.AddContactViewListener {
 
 	Contact model;
-	AddContactViewImpl view;
+	//AddContactViewImpl view;
+	AddContactView view;
 	ContactRepository ContactRepository;
+	ContactValidate validateContact;
 	Contact contact = new Contact();
 	
-	public AddContactPresenter(AddContactViewImpl view, ContactRepository ContactRepository){
+	public AddContactPresenter(AddContactView view, ContactRepository ContactRepository){
 		this.model = model;
 		this.view = view;
 		this.ContactRepository = ContactRepository;
 		this.contact = new Contact();
+		validateContact = new ContactValidate();
 		view.addListener(this);
 	}
 	
 	public void buttonClick() {
 		//get Form-input and save Contact
-		if(view.checkInput() == true){
+		/*if(view.checkInput() == true){
 			view.setNotification(false);
 			contact.createContact(view.getFirstName(), view.getName(), view.getStreet(), view.getPLZ(), view.getCity(), view.getPhone(), view.getMobile(), view.getMail(),view.getBirthday());
 			ContactRepository.save(contact);
 		}
 		else{
 			view.setNotification(true);
+		}*/
+		//validateContact.isValid(view.getLayout().getTxt_FirstName(), view.getLayout().getTxt_Name(), view.getLayout().getTxt_Street(), view.getLayout().getTxt_PLZ(), view.getLayout().getTxt_City(), view.getLayout().getTxt_Phone(), view.getLayout().getTxt_Mobile(), view.getLayout().getTxt_Mail(),view.getLayout().getDf_Birhday());
+		
+		
+		if(
+				validateContact.isValid(view.getLayout().getTxt_FirstName(), view.getLayout().getTxt_Name(), view.getLayout().getTxt_Street(), view.getLayout().getTxt_PLZ(), view.getLayout().getTxt_City(), view.getLayout().getTxt_Phone(), view.getLayout().getTxt_Mobile(), view.getLayout().getTxt_Mail(),view.getLayout().getDf_Birhday())
+		)		
+		{
+			contact.createContact(view.getFirstName(), view.getName(), view.getStreet(), view.getPLZ(), view.getCity(), view.getPhone(), view.getMobile(), view.getMail(),view.getBirthday());
+			ContactRepository.save(contact);
+			Notification.show("Contact successfully saved");
 		}
+			
+			
+			
 	}
 
 	public void loadContact(Contact contact) {
